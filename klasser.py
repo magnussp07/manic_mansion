273ddd77f5c08499ce70b6f51794899b07d73bd2
magnussp07 <magnussp07@outlook.com>
2026-02-24 13:23:36 +0100

    c

---
+++ klasser.py
@@ -0,0 +1,17 @@
+
+
+class SpillObjekt:
+    pass
+
+class Spokelse(SpillObjekt):
+    pass
+
+class Spiller(SpillObjekt):
+    pass
+
+class Hindring(SpillObjekt):
+    pass
+
+class Sau(SpillObjekt):
+    pass
+
