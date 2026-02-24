d960f0a4556bc1d3ec89e775a9b9450de6b8f722
magnussp07 <magnussp07@outlook.com>
2026-02-24 13:17:42 +0100

    x

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import pygame as pg
+from constants import *
 
+pg.init()
+vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
+clock = pg.time.Clock()
+
+running = True
+
+while running:
+    
+    vindu.fill(WHITE)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        
+   
+    pg.display.flip()
+    clock.tick(FPS)
+
+pg.quit()
