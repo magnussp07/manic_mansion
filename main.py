e0a5c87695715f89509bfec0961378297a4578f2
magnussp07 <magnussp07@outlook.com>
2026-02-24 13:47:32 +0100

    s

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame as pg
+
 from constants import *
+from klasser import *
+
 
 pg.init()
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 clock = pg.time.Clock()
 
 running = True
+
+s1 = Spokelse(200, 300)
+s2 = Spokelse(400, 100)
+spokelser = [s1,s2]
 
 while running:
     
@@ -14,6 +21,10 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+            
+    for s in spokelser:
+        s.oppdater()
+        s.tegn()
         
    
     pg.display.flip()
