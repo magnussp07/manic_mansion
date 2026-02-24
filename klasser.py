3d9af85038859d78a5a19cb36cbcb5fe187ebbb6
m-nyberg <marienyberg07@gmail.com>
2026-02-24 13:48:14 +0100

    Spokelse

--- klasser.py
+++ klasser.py
@@ -1,4 +1,6 @@
 
+FRIVENSTRE = 200
+FRIHOYRE = 700
 
 class SpillObjekt:
     def __init__(self, x:int, y:int):
@@ -6,7 +8,24 @@ class SpillObjekt:
         self.y = y
 
 class Spokelse(SpillObjekt):
-    pass
+    def __init__(self, x:int, y: int) -> None:
+        super().__init__(x, y)
+        self.vx = 5
+        self.vy = 5
+
+    def oppdater(self):
+        self.x += self.vx
+            self.y += self.vy
+            if self.x > FRIHOYRE:
+                self.vx *= -1 # Snu farten (x-retning)
+            if self.x < FRIVENSTRE:
+                self.vx *= -1 # Snu farten (x-retning)        
+            if self.y > VINDUH:
+                self.vy *= -1 # Snu farten (y-retning)
+                self.y = hoyde - self.radius # Tving den ut fra kanten
+            if self.y < self.radius:
+                self.vy *= -1 # Snu farten (y-retning)  
+        
 
 class Spiller(SpillObjekt):
     pass
