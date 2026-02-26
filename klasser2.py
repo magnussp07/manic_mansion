94a17a3a19003f0e7a1263a5228d84bc2e6532b9
Maggie1111111 <margrethe.koller@outlook.com>
2026-02-26 11:03:59 +0100

    bruker konstanter

--- klasser2.py
+++ klasser2.py
@@ -9,7 +9,7 @@ class Spillbrett:
         self.running = True
         
         # Må legge inn tilfeldige posisjoner som ikke overlapper 
-        self.spokelser = [Spokelse(randint(100, 700), randint(50, 350)), Spokelse(randint(100, 700), randint(50, 350))]
+        self.spokelser = [Spokelse(randint(GRENSE_V, GRENSE_H), randint(50, VINDU_HOYDE)), Spokelse(randint(GRENSE_V, GRENSE_H), randint(50, VINDU_HOYDE))]
         self.sauer = [Sau(800, 50), Sau(800, 200), Sau(800, 350)]
         self.hindringer = [Hindring(180, 150), Hindring(580, 280), Hindring(420, 400)]
         self.spiller = Spiller(50, int(VINDU_HOYDE/2))
